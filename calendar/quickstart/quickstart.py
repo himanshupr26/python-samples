7c28da6ebaa7b0e5907fef8f1b4f2605129887d7
himanshupr <himanshu@workspacesamples.dev>
2021-11-30 17:35:19 +0000

    Added try-except block

--- calendar/quickstart/quickstart.py
+++ calendar/quickstart/quickstart.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,56 +12,77 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START calendar_quickstart]
+#
+# [START drive_quickstart]
 from __future__ import print_function
-import datetime
 import os.path
+from datetime import datetime
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
     """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+    try:
+        try:
+            if os.path.exists('token.json'):
+                creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        except FileExistsError as err:
+            print(err)
+        # If there are no (valid) credentials available, let the user log in.
+        try:
+            if not creds or not creds.valid:
+                try:
+                    if creds and creds.expired and creds.refresh_token:
+                        creds.refresh(Request())
+                except Exception as err:
+                    print('Invalid Grant', err)
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        'credentials.json', SCOPES)
+                    creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+                with open('token.json', 'w') as token:
+                    token.write(creds.to_json())
+        except FileNotFoundError as err:
+            print('File does not exist', err)
+
+        finally:
+            service = build('calendar', 'v3', credentials=creds)
+
+            # Call the Calendar API
+            now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+            print('Getting the upcoming 10 events')
+            events_result = service.events().list(calendarId='primary', timeMin=now,
+                                                  maxResults=10, singleEvents=True,
+                                                  orderBy='startTime').execute()
+            events = events_result.get('items', [])
+
+        try:
+            if not events:
+                print('No upcoming events found.')
+        except FileNotFoundError as err:
+            print('File does not exist', err)
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+            print('Events:')
+            for event in events:
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                print(start, event['summary'])
 
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    except Exception as err:
+        print(err)
 
 
 if __name__ == '__main__':
--- gmail/quickstart/quickstart.py
+++ gmail/quickstart/quickstart.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,32 +31,49 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    try:
+        try:
+            if os.path.exists('token.json'):
+                creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        except FileExistsError as err:
+            print(err)
+        # If there are no (valid) credentials available, let the user log in.
+        try:
+            if not creds or not creds.valid:
+                try:
+                    if creds and creds.expired and creds.refresh_token:
+                        creds.refresh(Request())
+                except Exception as err:
+                    print('Invalid Grant', err)
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        'credentials.json', SCOPES)
+                    creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+                with open('token.json', 'w') as token:
+                    token.write(creds.to_json())
+        except FileNotFoundError as err:
+            print('File does not exist', err)
 
-    service = build('gmail', 'v1', credentials=creds)
+        finally:
+            service = build('gmail', 'v1', credentials=creds)
 
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+            # Call the Gmail API
+            results = service.users().labels().list(userId='me').execute()
+            labels = results.get('labels', [])
 
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+            try:
+                if not labels:
+                    print('No labels found.')
+            except FileNotFoundError as err:
+                print('File does not exist', err)
+            else:
+                print('Labels:')
+                for label in labels:
+                    print(label['name'])
+    except Exception as err:
+        print(err)
+
 
 if __name__ == '__main__':
     main()
